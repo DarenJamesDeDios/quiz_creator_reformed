8d4aa175bf9ba5fe8124e71e28b07063f6422b9b
DyrenJmsu <darenjamesdedios@gmail.com>
2025-05-23 12:19:53 +0800

    Added .gitignore to ignore pycache when saving, Grouped specific function for organization and added classes to make them useful for importing in the main file, I also change some variable names and file names to make it etailed as much as possible.

--- main.py
+++ main.py
@@ -1,2 +1,33 @@
 #Planning to put the main menu here 
-#Where the user could make the choice of having to create the quiz or run the quiz
+#Where the user could make the choice of having to create the quiz or run the quiz  
+from quiz_create_questions import QuestionCreator
+from quiz_run_program import QuizGame
+import time
+from colorama import init, Fore, Style
+
+init(autoreset=True)
+
+def main():
+    while True:
+        print(Fore.BLUE + Style.BRIGHT + "\n======= Main Menu =======")
+        print(Fore.YELLOW + "1. Create Quiz Questions")
+        print(Fore.GREEN + "2. Play the Quiz")
+        print(Fore.RED + "3. Exit")
+
+        choice = input("Enter your choice (1/2/3): ").strip()
+
+        if choice == '1':
+            creator = QuestionCreator()
+            creator.run()
+        elif choice == '2':
+            game = QuizGame()
+            game.start()
+        elif choice == '3':
+            print("Goodbye!")
+            time.sleep(1)
+            break
+        else:
+            print(Fore.RED + "Invalid choice. Please select 1, 2, or 3.")
+
+if __name__ == "__main__":
+    main()
--- quiz_create_questions.py
+++ quiz_create_questions.py
@@ -1,76 +1,64 @@
 import time
 from colorama import init, Fore, Style
 
-# Initialize colorama
 init(autoreset=True)
 
-filename = "quiz_questions.txt"
+class QuestionCreator:
+    def __init__(self, filename="quiz_questions.txt"):
+        self.filename = filename
 
-def welcome_message():
-    print(Fore.YELLOW + "========" + Style.BRIGHT + "Welcome to Quiz Creator" + Style.RESET_ALL + Fore.YELLOW + "=========")
-    time.sleep(2)
+    def run(self):
+        self.welcome()
+        while True:
+            question = self.get_question()
+            choice_a, choice_b, choice_c, choice_d = self.get_options()
+            correct = self.get_correct_answer()
+            self.save_to_file(question, choice_a, choice_b, choice_c, choice_d, correct)
 
-def get_question():
-    print(Fore.YELLOW + "Please enter your quiz question.")
-    return input(Fore.YELLOW + "Q: ")
+            if not self.ask_continue():
+                break
 
-def get_options():
-    print(Fore.YELLOW + "Please input the 4 possible answers.")
-    choice_a = input("For letter a: ")
-    choice_b = input("For letter b: ")
-    choice_c = input("For letter c: ")
-    choice_d = input("For letter d: ")
-    return choice_a, choice_b, choice_c, choice_d
+    def welcome(self):
+        print(Fore.YELLOW + "========" + Style.BRIGHT + " Welcome to Quiz Creator " + Style.RESET_ALL + Fore.YELLOW + "========")
+        time.sleep(1)
 
-def get_correct_answer():
-    while True:
-        answer = input("The correct answer? (a, b, c, or d): ").lower()
-        if answer in ['a', 'b', 'c', 'd']:
-            print(Fore.YELLOW + "Saving the question and answer...")
-            time.sleep(3)
-            print(Fore.GREEN + "Done!")
-            time.sleep(1)
-            return answer
-        else:
-            print(Fore.RED + "Invalid input, please choose only a, b, c, or d")
-            time.sleep(1)
+    def get_question(self):
+        print(Fore.YELLOW + "Enter your quiz question:")
+        return input(Fore.CYAN + "Q: ")
 
-def save_to_file(question, choice_a, choice_b, choice_c, choice_d, correct):
-    with open(filename, "a", encoding="utf-8") as file:
-        file.write(f"Q: {question}\n")
-        file.write(f"a. {choice_a}\n")
-        file.write(f"b. {choice_b}\n")
-        file.write(f"c. {choice_c}\n")
-        file.write(f"d. {choice_d}\n")
-        file.write(f"Correct Answer: {correct}\n\n")
-        file.write("=" * 50 + "\n\n")
+    def get_options(self):
+        print(Fore.YELLOW + "Enter 4 options:")
+        choice_a = input("a: ")
+        choice_b = input("b: ")
+        choice_c = input("c: ")
+        choice_d = input("d: ")
+        return choice_a, choice_b, choice_c, choice_d
 
-def ask_continue():
-    while True:
-        print(Fore.YELLOW + "Do you want to add another quiz question?")
-        response = input(Fore.GREEN + "yes" + Style.RESET_ALL + " or " + Fore.RED + "no" + Style.RESET_ALL + "?: ").lower()
-        if response == 'yes':
-            return True
-        elif response == 'no':
-            print(Fore.YELLOW + "Exiting...")
-            time.sleep(3)
-            print("Your quiz questions are now " + Fore.GREEN + "saved" + Style.RESET_ALL + ".")
-            return False
-        else:
-            print("Only " + Fore.GREEN + "yes" + Style.RESET_ALL + " or " + Fore.RED + "no" + Style.RESET_ALL + " answers are allowed, my friend.")
-            time.sleep(1)
+    def get_correct_answer(self):
+        while True:
+            answer = input("Correct answer (a/b/c/d): ").lower()
+            if answer in ['a', 'b', 'c', 'd']:
+                print(Fore.YELLOW + "Saving the question...")
+                time.sleep(1)
+                return answer
+            print(Fore.RED + "Invalid input.")
 
-# Main program loop
-def main():
-    welcome_message()
-    while True:
-        question = get_question()
-        choice_a, choice_b, choice_c, choice_d = get_options()
-        correct = get_correct_answer()
-        save_to_file(question, choice_a, choice_b, choice_c, choice_d, correct)
-        if not ask_continue():
-            break
+    def save_to_file(self, q, a, b, c, d, correct):
+        with open(self.filename, "a", encoding="utf-8") as file:
+            file.write(f"Q: {q}\n")
+            file.write(f"a. {a}\n")
+            file.write(f"b. {b}\n")
+            file.write(f"c. {c}\n")
+            file.write(f"d. {d}\n")
+            file.write(f"Correct Answer: {correct}\n\n")
+            file.write("=" * 50 + "\n\n")
 
-# Start the program
-if __name__ == "__main__":
-    main()
+    def ask_continue(self):
+        while True:
+            answer = input("Add another question? (yes/no): ").lower()
+            if answer == 'yes':
+                return True
+            elif answer == 'no':
+                print(Fore.GREEN + "Questions saved!")
+                return False
+            print(Fore.RED + "Please enter 'yes' or 'no'.")
